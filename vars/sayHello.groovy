11eb7d615ce46f9c963c82f4b4a522391d2354cc
pushkarroushan <pushkarroushancs@gmail.com>
2018-02-05 00:44:07 +0530

    sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,7 +1,6 @@
 #!/usr/bin/env groovy
 
 def call(String name = 'human') {
-  echo "We at OpsTree aim to be the earth’s most focused Ops company.
-In doing so we wish to ensure that we allow all the creative minds to focus on creation., ${name}."
+  echo "Hello, ${name}."
 }
 
