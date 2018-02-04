15868e3bf3c7d7491d4fb0534fe08e7e1ec0ffd9
pushkarroushan <pushkarroushancs@gmail.com>
2018-02-05 01:27:10 +0530

    commited

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def call(String name = 'opstree solution') {
-  echo "Our Mission statement is relay on us, ${name}."
+def call(String name = 'pushkar roushan') {
+  echo ", ${name}."
 }
 
