60f5616940973a4fba0bd57835a8459a28319bd1
pushkarroushan <pushkarroushancs@gmail.com>
2018-02-05 00:52:45 +0530

    sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def call(String name = 'human') {
-  echo "Hello, ${name}."
+def call(String name = 'opstree solution') {
+  echo "Our Mission statement is relay on us, ${name}."
 }
 
